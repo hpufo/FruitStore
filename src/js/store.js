b9644aa84a8f5f4a03ea71143ce0f14f26b5d6bc
Christopher Stephenson <hpufo2@gmail.com>
2017-08-12 15:29:33 -0400

    Updated and cleaned up the project

--- src/js/store.js
+++ src/js/store.js
@@ -1,9 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-//import promise from 'redux-promise-middleware';
-import logger from 'redux-logger';
 import allReducers from './reducers';
 
-const middleware = applyMiddleware(thunk/*,logger()*/);
+const middleware = applyMiddleware(thunk);
 
 export default createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), middleware);
